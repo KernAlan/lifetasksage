fd4b5ef2bbf474604a1456983cd60bfa166a048e
Alan Kern <kernalan@gmail.com>
2023-12-16 23:53:02 -0600

    Add morgan error handling middleware

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import todoRoutes from './routes/todoRoutes';
 import cors from 'cors';
 import helmet from 'helmet';
 import rateLimit from 'express-rate-limit';
+import morgan from 'morgan';
 
 const app = express();
 const limiter = rateLimit({
@@ -14,6 +15,11 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(limiter);
+app.use(morgan('dev'));
+app.use((err: any, req: any, res: any, next: any) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.use('/api/todos', todoRoutes);
 
