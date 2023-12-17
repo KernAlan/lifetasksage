1eaf08f096ee1e34de371dee9691ba89cff0e6c0
Alan Kern <kernalan@gmail.com>
2023-12-16 23:51:13 -0600

    Add small rate limiter using express rate limit.

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,17 @@ import express from 'express';
 import todoRoutes from './routes/todoRoutes';
 import cors from 'cors';
 import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+app.use(limiter);
 
 app.use('/api/todos', todoRoutes);
 
