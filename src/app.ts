bb9ee62f0080ca5673ce79645a5680598d3b364d
Alan Kern <kernalan@gmail.com>
2023-12-17 00:38:42 -0600

    Beef up error handling for API.

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,43 @@
-import express, { Express } from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
-import rateLimit from 'express-rate-limit';
-import morgan from 'morgan';
-import todoRoutes from './routes/todoRoutes';
+import express, { Express } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+import morgan from "morgan";
+import todoRoutes from "./routes/todoRoutes";
 
 export function createApp(): Express {
-    const app = express();
-    const limiter = rateLimit({
-      windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 100, // limit each IP to 100 requests per windowMs
-    });
+  const app = express();
+  const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  });
 
-    app.use(express.json());
-    app.use(cors());
-    app.use(helmet());
-    app.use(limiter);
-    app.use(morgan('dev'));
+  app.use(express.json());
+  app.use(cors());
+  app.use(helmet());
+  app.use(limiter);
+  app.use(morgan("dev"));
 
-    app.use('/api/todos', todoRoutes);
+  app.use("/api/todos", todoRoutes);
 
-    app.use((err: any, req: any, res: any, next: any) => {
+  app.use(
+    (
+      err: any,
+      req: express.Request,
+      res: express.Response,
+      next: express.NextFunction
+    ) => {
       console.error(err.stack);
-      res.status(500).send('Something broke!');
-    });
 
-    return app;
+      const statusCode = err.statusCode || 500;
+
+      res.status(statusCode).json({
+        success: false,
+        message:
+          err.message || "An unexpected error occurred. Please try again.",
+      });
+    }
+  );
+
+  return app;
 }
--- src/controllers/todoController.ts
+++ src/controllers/todoController.ts
@@ -1,65 +1,100 @@
-// controllers/controller.ts
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import Todo from "../models/todoModel";
 
-export const getAllTodos = async (req: Request, res: Response) => {
+export const getAllTodos = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const todos = await Todo.findAll();
     res.json({ success: true, data: todos });
   } catch (error: any) {
-    res.status(500).json({ success: false, message: error.message });
+    next({ status: 500, message: error.message, stack: error.stack });
   }
 };
 
-export const getTodo = async (req: Request, res: Response) => {
+export const getTodo = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const todo = await Todo.findOne({ where: { id: req.params.id } });
+    if (!todo) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Todo not found" });
+    }
     res.json({ success: true, data: todo });
   } catch (error: any) {
-    res.status(500).json({ success: false, message: error.message });
+    next({ status: 500, message: error.message, stack: error.stack });
   }
 };
 
-export const createTodo = async (req: Request, res: Response) => {
+export const createTodo = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const { title } = req.body;
+    if (!title) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Title is required" });
+    }
     const newTodo = await Todo.create({ title });
-    res.status(201).json(newTodo);
+    res.status(201).json({ success: true, data: newTodo });
   } catch (error: any) {
-    res.status(500).send(error.message);
+    next({ status: 500, message: error.message, stack: error.stack });
   }
 };
 
-export const updateTodo = async (req: Request, res: Response) => {
+export const updateTodo = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const { id } = req.params;
     const { title, completed } = req.body;
 
     const todo = await Todo.findByPk(id);
-    if (todo) {
-      todo.title = title || todo.title;
-      todo.completed = completed !== undefined ? completed : todo.completed;
-      await todo.save();
-      res.json(todo);
-    } else {
-      res.status(404).send("Todo not found");
+    if (!todo) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Todo not found" });
     }
+
+    todo.title = title || todo.title;
+    todo.completed = completed !== undefined ? completed : todo.completed;
+    await todo.save();
+    res.json({ success: true, data: todo });
   } catch (error: any) {
-    res.status(500).send(error.message);
+    next({ status: 500, message: error.message, stack: error.stack });
   }
 };
 
-export const deleteTodo = async (req: Request, res: Response) => {
+export const deleteTodo = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     const { id } = req.params;
     const todo = await Todo.findByPk(id);
-    if (todo) {
-      await todo.destroy();
-      res.status(200).send("Todo deleted successfully");
-    } else {
-      res.status(404).send("Todo not found");
+    if (!todo) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Todo not found" });
     }
+
+    await todo.destroy();
+    res
+      .status(200)
+      .json({ success: true, message: "Todo deleted successfully." });
   } catch (error: any) {
-    res.status(500).send(error.message);
+    next({ status: 500, message: error.message, stack: error.stack });
   }
 };
