17a420aeba90c17318952a4d2b1aff5c319ccb03
Alan Kern <kernalan@gmail.com>
2023-12-16 23:49:43 -0600

    Add CORS support and Helmet.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 // index.ts
 import express from 'express';
-import todoRoutes from './routes/routes';
+import todoRoutes from './routes/todoRoutes';
+import cors from 'cors';
+import helmet from 'helmet';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+app.use(helmet());
 
 app.use('/api/todos', todoRoutes);
 
